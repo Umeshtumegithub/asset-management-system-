7571c7bd596df9aa73d155e97cb42342280f1384
Umeshtumegithub <163958075+Umeshtumegithub@users.noreply.github.com>
2026-06-27 13:04:30 +0530

    Add 'assets' to installed apps and configure templates

--- settings.py
+++ settings.py
@@ -5,3 +5,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+INSTALLED_APPS = [
+    ...
+    'assets',
+]
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
+
+STATIC_URL = 'static/'
