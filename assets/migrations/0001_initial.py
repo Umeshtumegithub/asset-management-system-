503e2828e35e336fb6717df737739327f376cac3
Umeshtumegithub <163958075+Umeshtumegithub@users.noreply.github.com>
2026-06-27 19:56:21 +0530

    Refactor Asset model field definitions

--- assets/migrations/0001_initial.py
+++ assets/migrations/0001_initial.py
@@ -1,4 +1,4 @@
-from django.db import migrations, models
+ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -11,30 +11,45 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Asset',
             fields=[
-                ('id', models.BigAutoField(
-                    auto_created=True,
-                    primary_key=True,
-                    serialize=False,
-                    verbose_name='ID'
-                )),
-                ('asset_id', models.CharField(
-                    max_length=20,
-                    unique=True
-                )),
-                ('asset_name', models.CharField(
-                    max_length=100
-                )),
-                ('category', models.CharField(
-                    max_length=50
-                )),
-                ('status', models.CharField(
-                    choices=[
-                        ('Available', 'Available'),
-                        ('Assigned', 'Assigned'),
-                        ('Maintenance', 'Maintenance')
-                    ],
-                    max_length=20
-                )),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    ),
+                ),
+                (
+                    'asset_id',
+                    models.CharField(
+                        max_length=20,
+                        unique=True,
+                    ),
+                ),
+                (
+                    'asset_name',
+                    models.CharField(
+                        max_length=100,
+                    ),
+                ),
+                (
+                    'category',
+                    models.CharField(
+                        max_length=50,
+                    ),
+                ),
+                (
+                    'status',
+                    models.CharField(
+                        choices=[
+                            ('Available', 'Available'),
+                            ('Assigned', 'Assigned'),
+                            ('Maintenance', 'Maintenance'),
+                        ],
+                        max_length=20,
+                    ),
+                ),
             ],
         ),
     ]
