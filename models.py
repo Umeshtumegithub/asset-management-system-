c67aa2c01613a8b567e784f09204549caba9527f
Umeshtumegithub <163958075+Umeshtumegithub@users.noreply.github.com>
2026-06-27 12:48:48 +0530

    Add Asset model with fields and status choices

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Asset(models.Model):
+    asset_id = models.CharField(max_length=20, unique=True)
+    asset_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+
+    STATUS_CHOICES = (
+        ('Available', 'Available'),
+        ('Assigned', 'Assigned'),
+        ('Maintenance', 'Maintenance'),
+    )
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.asset_name
